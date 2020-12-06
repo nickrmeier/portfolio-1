ffd16bf335c39e89c04fccc3a90fe09cd1328c07
nickrmeier <nickrmeier@gmaill.com>
2020-12-06 12:08:12 -0800

    form submit data sending to cvs file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -7,19 +8,21 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        file = database.write(f'\n\n\n{email}\n{subject}\n{message}')
+        csv_writer = csv.writer(database2, delimiter=",",
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
