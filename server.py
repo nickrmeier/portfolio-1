7ec4e579c2842033965a81c830b75816b94e40ef
nickrmeier <nickrmeier@gmaill.com>
2020-12-06 11:41:07 -0800

    paths working, email for submit working

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n\n\n{email}\n{subject}\n{message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
