9c079c4da4f8beb98a5416a102fa2fb881cb32b5
nickrmeier <nickrmeier@gmaill.com>
2020-12-05 17:59:25 -0800

    HTML templates added

--- server.py
+++ server.py
@@ -2,16 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/blog/dogs')
-def blog2():
-    return 'This is the dog blog'
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
